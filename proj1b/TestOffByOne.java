b294b5faa08782ce6fe6f9c7d8a0ec5b99c03ea1
visitorinhanshio <2223544759@qq.com>
2025-08-24 10:54:42 +0800

    finish proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,20 +2,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-    /*
+    
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
 
     // Your tests go here.
-    Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
     @Test
     public void testOffByOne(){
-        OffByOne offByOne=new OffByOne();
-        assertTrue(offByOne.equalChars('a', 'b'));
-        assertTrue(offByOne.equalChars('r', 'q'));
-        assertFalse(offByOne.equalChars('z', 'a'));
-        assertFalse(offByOne.equalChars('a', 'a'));
-        assertFalse(offByOne.equalChars('a', 'e'));
+        OffByOne obo=new OffByOne();
+        assertTrue(obo.equalChars('a', 'b'));
+        assertTrue(obo.equalChars('r', 'q'));
+        assertFalse(obo.equalChars('z', 'a'));
+        assertFalse(obo.equalChars('a', 'a'));
+        assertFalse(obo.equalChars('a', 'e'));
     }
 }
